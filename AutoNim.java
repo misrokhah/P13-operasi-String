5d0a370441a54f2ff7833e514fdaec48f05cbf60
misrokhah <32092489+misrokhah@users.noreply.github.com>
2017-12-12 16:04:50 +0700

    Add files via upload

---
+++ AutoNim.java
@@ -0,0 +1,14 @@
+public class AutoNim{
+
+    public static void main(String[] args){
+        //NIM : Tahun masuk + Kode Prodi + Nomor Urut Registrasi
+        String ta = "2015";
+        String kd_prodi = "153";
+        String no_reg = "0001";
+        String nim;
+
+        nim = ta.substring(2,4)+kd_prodi+no_reg;
+
+        System.out.println("NIM : "+nim);
+    }
+}
